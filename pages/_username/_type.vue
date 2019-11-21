df265b679d4e6942f20718fbf96aca9374769e4d
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-11-21 16:50:18 +0500

    basic pages setup

---
+++ pages/_username/_type.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ username }} | {{ type }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: this.$route.params.username,
+      type: this.$route.params.type
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/_username/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ username }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: this.$route.params.username
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
