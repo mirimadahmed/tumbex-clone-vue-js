a1ac6ff98d16e5690ab05da4b752d272fcaf4b1d
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-12-18 16:40:48 +0500

    favs displayed

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,37 @@
 <template>
-  <div />
+  <div class="main">
+    <FavBlogs />
+    <FavPosts />
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import api from '@/api'
+import FavBlogs from '@/components/Layout/FavBlogs'
+import FavPosts from '@/components/Layout/FavPosts'
 export default {
-
+  components: {
+    FavBlogs,
+    FavPosts
+  },
+  computed: {
+    ...mapGetters(['isLoggedIn', 'user', 'favPosts'])
+  },
+  created () {
+    this.fetch()
+  },
+  methods: {
+    async fetch () {
+      const { data } = await api.myFavs(this.user.user_id)
+      this.$store.dispatch('setBlog', data)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.main {
+  background-color: #35444c !important;
+}
 </style>
